338fbe70030d2ccf9849088785d9489c1dcee3a8
Caroline Orsi <caroline.orsi@gmail.com>
2014-10-19 21:07:50 -0700

    Added feature to only select customers who have ordered 20 or more watermelons.

--- call.py
+++ call.py
@@ -21,7 +21,7 @@ def connect_to_db(db_name):
 	DB = CONN.cursor()
 
 def get_next_customer():
-	query = """ SELECT customer_id, first, last, telephone FROM customers WHERE called = '' """
+	query = """ SELECT customer_id, first, last, telephone FROM customer_orders WHERE called = '' AND watermelons >= 20 """
 	DB.execute(query, )
 	row = DB.fetchone()
 	return row
@@ -35,43 +35,17 @@ def display_customer(row):
 	print "\n"
 
 def update_records(row):
-	date = "10/19/14"
+	date = time.strftime('%m/%d/%Y')
 	query = """ UPDATE customers SET called = ? WHERE customer_id = ? """
 	DB.execute(query, (date, row[0]))
 	CONN.commit()
 
-# def update_customer_file(filename, customer_id):
-# 	lines = open(filename, 'r').readlines()
-
-# 	for i in range(len(lines)):
-# 		if lines[i].startswith(customer_id):
-# 			lines[i] = lines[i][0:-1] + time.strftime('%m/%d/%Y') + '\n'
-
-# 	out = open(filename, 'w')
-# 	out.writelines(lines)
-
-	# out.close()
 
 def main():
 	connect_to_db("melons.db")
 	customer = get_next_customer()
 	display_customer(customer)
 	update_records(customer)
-
-	# # Load data from our csv files
-	# customers = load_customers('customers.csv')
-	# orders    = load_orders('orders.csv')
-
-	# # Loop through each order
-	# for order in orders:
-	# 	# Is this order over 20 watermelon?
-	# 	if order.get('num_watermelons', 0) > 20:
-	# 		# Has this customer not been contacted yet?
-	# 		customer = customers.get(order.get('customer_id', 0), 0)
-	# 		if customer.get('called', '') == '':
-	# 			display_customer(customer)
-	# 			update_customer_file('customers.csv', customer['customer_id'])
-	# 			break
 
 if __name__ == '__main__':
 	main()
